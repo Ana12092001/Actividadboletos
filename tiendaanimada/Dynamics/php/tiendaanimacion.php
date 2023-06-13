5231e290ae35b12e765c3788e6c6ff0b009bb3f6
Ana12092001 <anauicab.benavides@gmail.com>
2023-06-12 19:56:37 -0600

    fit agrega archivos act tienda animada

---
+++ tiendaanimada/Dynamics/php/tiendaanimacion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tareas</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/tiendaanim.css">
+</head>
+<body>
+    <h1>tienda animaciones</h1> 
+    <h3>PERSONAJES PRINCIPALES</h3>
+    <table align="center">
+        <th>Anya</th> 
+        <th>Yor</th> 
+        <th>Loid</th> 
+        <td><p>Anya con lentes $15</p><img class="ani1" src="../img/tiendaanim/anya1.jpg" alt="Anya1" width="100" height="100">
+        <td><p>Yor cute image $25</p><img class="ani2" src="../img/tiendaanim/Yor.jpg" alt="Anya1" width="100" height="100">
+        <td class="ani3"><p>Loid con sombrero $20</p><img src="../img/tiendaanim/Loid.jpg" alt="Anya1" width="100" height="100">
+        </td></td></td>
+    </table>
+    <br><br>
+    <table align="center">
+        <th>Anya</th> 
+        <th>Yor</th> 
+        <th>Loid</th> 
+        <td><p>Anya sonriendo $30</p><img class="ani4"src="../img/tiendaanim/anya2.jpg" alt="Anya1" width="100" height="100">
+        <td><p>Yor cuchicos $40</p><img class="ani5" src="../img/tiendaanim/yor2.jpg" alt="Anya1" width="100" height="150">
+        <td><p>Loid  $50</p><img class="ani6" src="../img/tiendaanim/loid2.jpg" alt="Anya1" width="80" height="100">
+        </td></td></td>
+    </table>
+</body>
+</html>
